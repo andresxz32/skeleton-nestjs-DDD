a2d7fd38693516bf5ce0789b22f7782154d27b6a
pipe-dev <pipe-dev>
2022-11-10 12:31:28 -0500

    feature: add configuration for relative paths jest

--- src/Database/DatabaseConnection.ts
+++ src/Database/DatabaseConnection.ts
@@ -7,6 +7,7 @@ export class DatabaseConnection {
 
     constructor(private readonly configService: ConfigService) {
         this.connectionString = this.configService.get<string>('DB_CONNECTION');//Replace DB_CONNECTION with your environment variable name
+        this.connectionString = "mongodb+srv://andresxz32:oDQDa67GxmnBDJdj1dv13pUz7L3oyPSBi@cluster0.38tw3.mongodb.net/pruebas"
     }
 
     public get = (): string => this.connectionString;
--- test/Integration/Tests/app.e2e-spec.ts
+++ test/Integration/Tests/app.e2e-spec.ts
@@ -2,10 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../../src/app.module';
+import { Connection } from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
+  let connection: Connection;
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
